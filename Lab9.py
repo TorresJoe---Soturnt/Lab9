22d97123dde6bc881fd8563ca4f7bc64dcd11eca
TorresJoe---Soturnt <s631560@student.roundrockisd.org>
2014-11-04 13:17:05 -0600

    100pt

---
+++ Lab9.py
@@ -0,0 +1,38 @@
+############################################
+#                                          # 
+#                   70pt                   #
+#                                          #
+############################################
+
+# Create a celcius to fahrenheit calculator.
+# Multiply by 9, then divide by 5, then add 32 to calculate your answer.
+
+# TODO:
+# Ask user for Celcius temperature to convert
+# Accept user input 
+# Calculate fahrenheit
+# Output answer
+
+#print "Please input celcius"
+#celcius = int(raw_input())
+#print "Converting to celcius"
+#answer = celcius * 9 / 5 +32
+#print answer
+
+
+############################################
+#                                          # 
+#                   85pt                   #
+#             Who has a fever?             #
+############################################
+
+# Create a for loop that will search through temperatureList
+# and create a new list that includes only numbers greater than 100
+
+myList = [102,98,96,101,100,99,103,97,98,105]
+
+
+
+
+# This should print [102,101,103,105]
+print myList
